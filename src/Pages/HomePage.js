13813229994044594c18301e3442eee099d23640
Justin Wang <justin.wang1ab@gmail.com>
2024-03-02 14:07:11 -0500

    add expansion feature

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -8,6 +8,7 @@ import axios from 'axios';
 const HomePage = () => {
   const instance = axios.create({ baseURL: 'https://dreamschools-182388d60dc9.herokuapp.com'});
   const [ schoolList, setSchoolList ] = useState([]);
+  const [ isExpanded, setIsExpanded ] = useState(false);
 
   useEffect(() => {
     instance.get('/getSchools', {count: 10}, {
@@ -26,17 +27,17 @@ const HomePage = () => {
       <Link to="/create" className="create-link">
         <button>Create</button>
       </Link>
-      <div className="school-list">
+      <div className="school-list" onClick={setIsExpanded(true)}>
         {schoolList.map((school, index) => (
-          <Link to={`/school/${school.id}`} key={index} className="school-card">
+          <div className="school-card">
             <div className="image-container">
                 <img src={school.blobURL} alt={school.name} className="school-image"/>
             </div>
             <div className="school-info">
               <h2 className="school-name">{school.name}</h2>
-              <p className="school-description">{school.about.slice(0, 100)}...</p>
+              <p className="school-description">{isExpanded ? school.about : school.about.slice(0, 100) + '...'}</p>
             </div>
-          </Link>
+          </div>
         ))}
       </div>
     </div>
